c2c1f491895daf69a3273cf04895400a341fe98c
Bastien <bastien.biblocque@gmail.com>
2023-06-28 16:28:20 +0200

    début du 4

---
+++ lib/service/generateBachelor.dart
@@ -0,0 +1,79 @@
+import 'package:finder/class/bachelor.dart';
+import 'package:faker/faker.dart';
+import 'dart:math';
+
+class BachelorService {
+  var faker = Faker();
+
+  List<String> firstNameMan = [
+    "Hugo",
+    "Ethan",
+    "Lucas",
+    "Gabriel",
+    "Arthur",
+    "Louis",
+    "Raphaël",
+    "Jules",
+    "Adam",
+    "Maël",
+    "Léo",
+    "Liam",
+    "Paul",
+    "Nathan",
+    "Gabin",
+    "Sacha",
+    "Noah",
+    "Mohamed",
+    "Tom",
+    "Aaron",
+    "Mathis",
+  ];
+
+  List<String> firstNameWoman = [
+    "Lyne",
+    "Léa",
+    "Lola",
+    "Inès",
+    "Jade",
+    "Louise",
+    "Lina",
+    "Léna",
+    "Mila",
+    "Rose",
+    "Ambre",
+    "Julia",
+    "Alice",
+    "Anna",
+    "Juliette",
+    "Zoé",
+    "Camille",
+    "Chloé",
+    "Léna",
+    "Léna",
+  ];
+
+  var rng = Random();
+
+  List<Bachelor> getBachelors() {
+    List<Bachelor> bachelors = [];
+
+    for (int i = 0; i < 15; i++) {
+      bachelors.add(Bachelor(
+          firstName: firstNameMan[rng.nextInt(firstNameMan.length)],
+          lastName: faker.person.lastName(),
+          gender: Gender.men,
+          description: faker.lorem.sentences(3).join(' '),
+          avatar: 'images/man-${i + 1}.png'));
+    }
+    for (int i = 0; i < 15; i++) {
+      bachelors.add(Bachelor(
+          firstName: firstNameWoman[rng.nextInt(firstNameWoman.length)],
+          lastName: faker.person.lastName(),
+          gender: Gender.women,
+          description: faker.lorem.sentences(3).join(' '),
+          avatar: 'images/woman-${i + 1}.png'));
+    }
+    bachelors.shuffle();
+    return bachelors;
+  }
+}
